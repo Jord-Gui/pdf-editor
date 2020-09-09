79e2f1c76c828584cbb5dabf5b30a78e15a3603e
Jord <jgui0003@student.monash.edu>
2020-09-09 15:46:16 +1000

    added laptop env

--- pdfenv/Scripts/activate.bat
+++ pdfenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Users\Jord\Repositories\PDF-Editor\pdfenv
+set VIRTUAL_ENV=C:\Users\Jord_\Repositories\PDF-Editor\pdfenv
 
 if not defined PROMPT set PROMPT=$P$G
 
