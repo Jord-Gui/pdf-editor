893029939ff2ebd533572b30661a3660259e487a
Jord Gui <jord_gui@hotmail.com>
2022-02-09 11:36:21 +1100

    Refactor code and start merge_files function

--- src/gui.py
+++ src/gui.py
@@ -1,3 +1,5 @@
+from cgitb import text
+from statistics import geometric_mean
 import tkinter
 from tkinter import filedialog
 
@@ -5,28 +7,41 @@ from merge import merge_pdf
 from cut import cut_pdf
 
 
-class PDFEditor:
+class PdfEditor:
 
-    def __init__(self, root) -> None:
-        root.title("Jord's PDF Editor")
-        root.geometry("400x300")
+    def __init__(self, master, title, geometry) -> None:
+        master.title(title)
+        master.geometry(geometry)
 
-        self.filenames = []
+        self.merge_btn = tkinter.Button(master, text='Merge Files', command=lambda: self.merge_files(master, geometry))
+        self.merge_btn.pack()
 
-        self.upload_btn = tkinter.Button(root, text='Open', command=self.UploadAction)
-        self.upload_btn.pack()
+    def open_new_window(self, parent, title, size):
+        new_window = tkinter.Toplevel(parent)
+        new_window.title(title)
+        new_window.geometry(size)
+        return new_window
 
-    def UploadAction(self):
-        filename = filedialog.askopenfilename()
+    def upload_action(self, filenames):
+        filename = filedialog.askopenfilenames()
         if filename:
             print('Selected:', filename)
-            self.filenames.append(filename)
+            filenames.append(filename)
         else:
             print('No file selected')
+        print(filenames)
+
+    def merge_files(self, master, geometry):
+        print('Merge selected')
+        merge_window = self.open_new_window(master, "Merge PDFs", geometry)
+
+        filenames = []
+
+        upload_btn = tkinter.Button(merge_window, text='Select Files', command=lambda: self.upload_action(filenames))
+        upload_btn.pack()
 
 
 root = tkinter.Tk()
-pdfEdit = PDFEditor(root)
+pdfEdit = PdfEditor(root, "Jord's PDF Editor", "400x300")
 root.mainloop()
 
-print(str(pdfEdit.filenames))
